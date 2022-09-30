dc83d4265d0147de53173ed596f27fb0764ccddc
Alex <al.repin.a@yandex.ru>
2022-09-30 19:38:45 +0300

    1 trial

---
+++ Task/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array = ["hello", "2", "world", ":-)"]
+{
+    for (int i = 0: i < array.Length; i ++)
+    {
+        string arr = array[i];
+        char[] ar = arr.ToCharArray();
+        j = ar.Length;
+        if (j <= 3)
+        {
+            Console.Write($"[{sring.Join(", ", arr)}]");
+        }
+
+    }
+}
